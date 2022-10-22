4f7c2d92a08a7c5c8feb7a60252864a5ff3280e9
Jnanesh124 <99884442+Jnanesh124@users.noreply.github.com>
2022-10-22 16:04:54 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -48,3 +48,8 @@ class Config(object):
 
     # To record start time of bot
     BOT_START_TIME = time.time()
+    
+    SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 10))
+SELF_DELETE = environ.get('SELF_DELETE', True)
+if SELF_DELETE == "True":
+    SELF_DELETE = True
